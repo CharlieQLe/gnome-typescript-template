adeddbd831c08730735809c74e8923d353222dc8
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-02-17 15:41:37 -0500

    Add rollup for bundling vendor files

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+
+/* Export like so:
+
+export default [
+    ...
+    {
+        input: 'path/to/module/js',
+        output: {
+            file: 'src/vendor/modulename.js,
+        },
+        plugins: [nodeResolve(), commonjs(), ...],
+    },
+    ...
+]
+
+*/
+
+export default [
+    {
+        
+    }
+];
