60abd3b06bfaf282a2723e30acd9ab2b316cd01b
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-02-17 13:00:56 -0500

    Updated template

--- src/application.ts
+++ src/application.ts
@@ -1,12 +1,10 @@
 import GObject from 'gi://GObject';
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 import Gtk from 'gi://Gtk?version=4.0';
 import Adw from 'gi://Adw?version=1';
 
 import { Window } from "resource:///io/github/charlieqle/GnomeTypescriptTemplate/js/widgets/window.js";
-
-pkg.initGettext();
-pkg.initFormat();
 
 export class Application extends Adw.Application {
     private _mainWindow: Window | null;
@@ -15,19 +13,14 @@ export class Application extends Adw.Application {
         GObject.registerClass(this);
     }
 
-    constructor() {
+    public constructor() {
         super({ application_id: pkg.name, flags: Gio.ApplicationFlags.FLAGS_NONE });
-
         this._mainWindow = null;
 
-        const quit_action = new Gio.SimpleAction({ name: 'quit' });
-        quit_action.connect('activate', _ => this.quit());
-        this.add_action(quit_action);
-        this.set_accels_for_action('app.quit', ['<primary>q']);
-
-        const show_about_action = new Gio.SimpleAction({ name: 'about' });
-        show_about_action.connect('activate', _ => {
-            const aboutWindow = new Adw.AboutWindow({
+        // Add actions
+        this._addAction('quit', _ => this.quit(), null);
+        this._addAction('about', _ => {
+            new Adw.AboutWindow({
                 transient_for: this._mainWindow,
                 application_name: 'GNOME Typescript Template',
                 application_icon: pkg.name,
@@ -35,13 +28,25 @@ export class Application extends Adw.Application {
                 version: pkg.version,
                 developers: ['Charlie Le'],
                 copyright: '© 2023 Charlie Le'
-            });
-            aboutWindow.present();
-        });
-        this.add_action(show_about_action);
+            }).present();
+        }, null);
+
+        // Set accels
+        this.set_accels_for_action('app.quit', ['<primary>q']);
     }
 
-    vfunc_activate() {
+    /// FUNCS
+
+    private _addAction(name: string, callback: (action: Gio.SimpleAction, ...params: any[]) => void, parameterType: GLib.VariantType<any> | null) {
+        const action = Gio.SimpleAction.new(name, parameterType);
+        action.connect('activate', callback);
+        this.add_action(action);
+        return action;
+    }
+
+    /// VFUNCS
+
+    public vfunc_activate() {
         if (this._mainWindow == null) {
             this._mainWindow = new Window(this);
         }
