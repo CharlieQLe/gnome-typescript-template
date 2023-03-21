06a301c07689470dfe38564ecaf7452644ce110d
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-03-21 12:21:23 -0400

    Update ts-for-gir settings

--- .ts-for-girrc.js
+++ .ts-for-girrc.js
@@ -1,4 +1,13 @@
 export default {
+    "buildType": "types",
+    "environments": [
+        "gjs"
+    ],
+    "generateAlias": true,
+    "modules": [
+        "*"
+    ],
+    "promisify": true,
     "ignore": [
         "GdkX11-3.0",
         "Gtk-3.0",
