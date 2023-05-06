7b70fb703de65d95285fa678fb409bc22c1ff125
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-05-06 12:16:08 -0400

    Use separate base and base-ambient packages

---
+++ @types/global.d.ts
@@ -0,0 +1,2 @@
+import "@gnome-flatpak-types/base";
+import "@gnome-flatpak-types/base-ambient";
--- scripts/generate_sources.sh
+++ scripts/generate_sources.sh
@@ -6,3 +6,5 @@ if ! command -v flatpak-node-generator > /dev/null ; then
 fi
 
 flatpak-node-generator yarn -r yarn.lock -o modules/yarn-deps/generated-sources.json
+
+# IF IN FLATPAK, USE `$XDG_DATA_HOME/python/bin/flatpak-node-generator`
