ad89431984feab83bae5c0a0026dfe248d033dfb
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-05-23 10:55:15 -0400

    Use @girs

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,2 +1,6 @@
-import "@gnome-flatpak-types/base";
-import "@gnome-flatpak-types/base-ambient";
+import "@girs/gjs";
+import "@girs/adw-1";
+import "@girs/gio-2.0";
+import "@girs/glib-2.0";
+import "@girs/gobject-2.0";
+import "@girs/gtk-4.0";
